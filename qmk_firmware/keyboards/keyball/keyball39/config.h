f37ba0b0d3ea547be1f912c63c2342a6d1eaa6c5
hirashin <shingo.hirasawa@gmail.com>
2024-12-30 02:21:06 +0900

    Modify

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -18,6 +18,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#include "config_common.h"
+
+// USB Device descriptor parameters
+#define VENDOR_ID           0x5957      // "YW" = Yowkees
+#define PRODUCT_ID          0x0200      
+#define DEVICE_VER          0x0001
+#define MANUFACTURER        Yowkees
+#define PRODUCT             Keyball39
+
 // Key matrix parameters
 #define MATRIX_ROWS         (4 * 2)  // split keyboard
 #define MATRIX_COLS         6
@@ -31,43 +40,34 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SOFT_SERIAL_PIN         D2
 #define SPLIT_HAND_MATRIX_GRID  F6, B5
 #define SPLIT_USB_DETECT
-#ifdef OLED_ENABLE
-#    define SPLIT_OLED_ENABLE
-#endif
-
-// If your PC does not recognize Keyball, try setting this macro. This macro
-// increases the firmware size by 200 bytes, so it is disabled by default, but
-// it has been reported to work well in such cases.
-//#define SPLIT_WATCHDOG_ENABLE
+#define SPLIT_USB_TIMEOUT       500
 
 #define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
 
 // RGB LED settings
-#define WS2812_DI_PIN       D3
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLED_NUM      48
-#    define RGBLED_SPLIT    { 24, 24 }  // (24 + 22)
-#    ifndef RGBLIGHT_LIMIT_VAL
-#        define RGBLIGHT_LIMIT_VAL  150 // limitated for power consumption
-#    endif
-#    ifndef RGBLIGHT_VAL_STEP
-#        define RGBLIGHT_VAL_STEP   15
-#    endif
-#    ifndef RGBLIGHT_HUE_STEP
-#        define RGBLIGHT_HUE_STEP   17
-#    endif
-#    ifndef RGBLIGHT_SAT_STEP
-#        define RGBLIGHT_SAT_STEP   17
-#    endif
-#endif
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_SPLIT    { 24, 24 }
-#endif
+// #define RGB_DI_PIN          D3k
+// #ifdef RGBLIGHT_ENABLE
+// #    define RGBLED_NUM      48
+// #    define RGBLED_SPLIT    { 24, 24 }  // (24 + 22)
+// #    ifndef RGBLIGHT_LIMIT_VAL
+// #        define RGBLIGHT_LIMIT_VAL  150 // limitated for power consumption
+// #    endif
+// #    ifndef RGBLIGHT_VAL_STEP
+// #        define RGBLIGHT_VAL_STEP   15
+// #    endif
+// #    ifndef RGBLIGHT_HUE_STEP
+// #        define RGBLIGHT_HUE_STEP   17
+// #    endif
+// #    ifndef RGBLIGHT_SAT_STEP
+// #        define RGBLIGHT_SAT_STEP   17
+// #    endif
+// #endif
+// #ifdef RGB_MATRIX_ENABLE
+// #    define RGB_MATRIX_SPLIT    { 24, 24 }
+// #endif
 
 #ifndef OLED_FONT_H
-#    define OLED_FONT_H "keyboards/keyball/lib/logofont/logofont.c"
-#    define OLED_FONT_START 32
-#    define OLED_FONT_END 195
+#    define OLED_FONT_H "keyboards/keyball/lib/glcdfont.c"
 #endif
 
 #if !defined(LAYER_STATE_8BIT) && !defined(LAYER_STATE_16BIT) && !defined(LAYER_STATE_32BIT)
@@ -77,3 +77,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
